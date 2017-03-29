5ec969a2ec4e8565b1c091bb5f2d89c33a47fbaf
rafael ramirez <francisco.ramirez@SofttekClients.com>
2017-03-29 17:39:38 -0600

    final

--- Proyecto01.java
+++ Proyecto01.java
@@ -4,7 +4,7 @@ public class Proyecto01 {
 
 public static void main(String[] args) {
 		
-		String hello = "Hello GitHub";
+		String hello = "Hello Git and GitHub, a bug was fixed!!";
 		
 		System.out.println(hello);
 
